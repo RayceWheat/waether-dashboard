539803fee0a66b77248e0fc8d8b5533634df4c07
RayceWheat <RayceWheat@gmail.com>
2021-09-18 16:53:32 -0500

    updated the display info funcationality

--- assests/js/js.js
+++ assests/js/js.js
@@ -1,3 +1,6 @@
+// Selceting the info
+var infoEl = document.getElementById('info');
+
 // Adding the a listener to the buttn
 $('#search').on("click", "button", function(){
   // seleceting the value inside the text 
@@ -9,11 +12,10 @@ $('#search').on("click", "button", function(){
   .addClass('searched-city')
   .text(userInput)
 
-  
   console.log(userInput);
 
   // making an apiCall formant with user input
-  var apiCall = 'http://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=ba69d1ea74461bab7d55a955393c7b3a';
+  var apiCall = 'http://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&units=imperial' + '&appid=ba69d1ea74461bab7d55a955393c7b3a';
 
   fetch(apiCall)
 .then(function(response){
@@ -29,17 +31,34 @@ $('#search').on("click", "button", function(){
       console.log(response);
       $('#saved-cities').append(newCity);
 
-    }
-        
+      console.log(response.coord.lat);
+      
+      apiCallOne = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + response.coord.lat
+       + '&lon=' + response.coord.lon + '&units=imperial' + '&appid=ba69d1ea74461bab7d55a955393c7b3a';
+
+      console.log(apiCallOne);
+
+      fetch(apiCallOne)
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(response){
+        console.log(response);
+
+        displayInfo(response);
+
+      })
+      }
+  })
 })
 
-})
 
+// Function which will display info 
 var displayInfo = function(response) {
-  var newInfo = (<)
-  
-  response.main.humidity
-  response.main.temp
 
-  $('#info')
+  infoEl.innerHTML += '<p>' + response.current.temp + '</p>';
+  infoEl.innerHTML += '<p>' + response.current.humidity + '</p>';
+  infoEl.innerHTML += '<p>' + response.current.wind_speed + '</p>';
+  infoEl.innerHTML += '<p>' + response.current.uvi + '</p>';
+
 }
